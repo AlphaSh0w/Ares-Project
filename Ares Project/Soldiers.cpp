574f915c637024cc10c55ed0cf463a357415b9c3
Alpha Show <lastblast@outlook.fr>
2020-02-26 21:39:53 +0100

    Added base abstract Soldier class.

---
+++ Ares Project/Soldiers.cpp
@@ -0,0 +1,7 @@
+#include "Soldiers.h"
+
+Soldier::Soldier(int hp, int sp, int def, int initiative)
+	:
+	stats(hp,sp,def,initiative)
+{
+}
---
+++ Ares Project/Soldiers.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Stats.h"
+
+class Soldier
+{
+public:
+	
+private:
+	
+protected:
+	Soldier(int hp, int sp, int def, int initiative);
+protected:
+	Stats stats;
+};
